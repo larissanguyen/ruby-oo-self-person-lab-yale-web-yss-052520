0b90248eee17ad54582c65f3c9269f414dbd8bd7
Larissa Nguyen <larissa.nguyen@yale.edu>
2020-05-27 20:01:24 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -76,8 +76,8 @@ class Person
     
     def start_conversation(person, topic)
       if topic == "politics"
-        self.happiness = (@happiness - 1)
-        person.happiness = (person.happiness - 1)
+        self.happiness = (@happiness - 2)
+        person.happiness = (person.happiness - 2)
         return "blah blah partisan blah lobbyist"
       elsif topic == "weather"
         self.happiness = (@happiness + 1)
