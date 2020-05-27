7737f69b225ffaf8674f09fdae05d1854dd85301
Larissa Nguyen <larissa.nguyen@yale.edu>
2020-05-27 19:53:55 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -72,6 +72,6 @@ class Person
     def call_friend(friend)
       self.happiness = (@happiness + 3)
       friend.happiness = (friend.happiness + 3)
-      return ""
+      return "Hi #{friend.name}"
     end
 end
